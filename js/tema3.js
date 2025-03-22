8c3100e406a87692069827c95246f94bb2294dfa
demchenko2009 <166243357+demchenko2009@users.noreply.github.com>
2025-03-22 15:38:15 +0100

    ls3

---
+++ js/tema3.js
@@ -0,0 +1,90 @@
+// const messegee = "i love java script";
+// console.log(messegee);
+// console.log(typeof(messegee));
+// console.log(messegee[14]);
+// console.log(messegee.length);
+// console.log(messegee[messegee.length-1]);
+
+// const userName="ivan";
+// const resalt = userName + " "+ messegee;
+// console.log(resalt);
+
+
+// const a = 5;
+// const d = 10
+// const b = "5"
+// const c = a + d + b
+// console.log(c);
+// console.log(typeof(c));
+
+// const user = "ivan"
+// const grupp = "FE_1Y_5_02__03_24"
+// const messegee = "i love java script";
+// const result = user + " "+ grupp + " " + messegee;
+// console.log(result);
+
+// console.log(`учень ${user} навчаеться в группи ${grupp} i ${messegee}`);
+
+
+
+
+// const mear = "як вас звуть"
+// console.log(mear.length);
+// console.log(mear.toLowerCase());
+// console.log(mear.toUpperCase());
+// console.log(mear.indexOf("звуть"));
+// console.log(mear.includes("вас"));
+
+
+// const maill  = "почта ивана vfdd44@gmail.com"
+// const resallt = maill.includes(".com")
+// console.log(resallt);
+// const resallt = maill.indexOf("vfdd44@gmail.com");
+// console.log(resallt);
+
+// const uber = "ivan krosava love java script"
+// console.log(uber.startsWith("krosava",5));
+// console.log(uber.endsWith("script"));
+
+
+
+
+
+
+
+// 1 Вивести result в консоль. Перевірити тип змінної result= 5+5+’5’.
+
+const a = 5
+const b = 5
+const c ="5"
+const result =  a + b + c;
+console.log( result);
+console.log( typeof (result));
+
+
+// 2 Створіть змінну email з вашою електронною адресою. Напишіть скрипт, який перевіряє чи містить змінна email символ @ і рахує загальну кількість символів. Результат виведіть в консоль.
+
+const emaill = "vanja888@gmail.com";
+const komm = emaill.includes(".com")
+console.log(komm);
+
+const resallt = ("vfdd44@gmail.com".length);
+console.log(resallt);
+
+
+// 3 Записати в окремих змінних кожне слово: My, name, is. Поєднати всі слова в змінну fullName. До змінної fullName додати ‘Viktor’. fullName вивести в консоль.
+
+ const names =" my"
+const useer = " neim "
+const answet = "is "
+const fullName =  names + useer+ answet + "viktor";
+console.log(fullName);
+
+// Створіть змінну userName з ім’ям користувача і змінну payment з сумою до оплати. За допомогою шаблонного рядка виведіть на екран через alert повідомлення на прикладі цього: «Дякуємо, Олександро! До сплати 300 гривень»
+
+const userName = "Nazar";
+const payment = "500$"
+ alert(`Дякуемо ${userName} до оплаты ${payment}`);
+
+
+
